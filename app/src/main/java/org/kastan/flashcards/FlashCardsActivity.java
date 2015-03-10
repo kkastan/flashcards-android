ff334780282fa670421a668980533726e7de3645
Karl Kastan <karlkastan@gmail.com>
2015-03-09 22:39:08 -0700

    fix indentation

--- app/src/main/java/org/kastan/flashcards/FlashCardsActivity.java
+++ app/src/main/java/org/kastan/flashcards/FlashCardsActivity.java
@@ -12,55 +12,55 @@ import java.util.Random;
 
 public class FlashCardsActivity extends ActionBarActivity {
 
-    Random rand = new Random();
+  Random rand = new Random();
 
-    static String[] words = {
-            "the", "a", "an", "here", "is",
-            "I", "see", "and", "we", "he",
-            "she", "big", "have", "it", "no",
-            "go", "you", "this", "down", "small"
-    };
+  static String[] words = {
+          "the", "a", "an", "here", "is",
+          "I", "see", "and", "we", "he",
+          "she", "big", "have", "it", "no",
+          "go", "you", "this", "down", "small"
+  };
 
-    private TextView wordView;
+  private TextView wordView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_flash_cards);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_flash_cards);
 
-        wordView = (TextView) findViewById(R.id.flash_card_word);
-        updateWord();
+    wordView = (TextView) findViewById(R.id.flash_card_word);
+    updateWord();
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_flash_cards, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_flash_cards, menu);
-        return true;
-    }
+    return super.onOptionsItemSelected(item);
+  }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+  public void nextWord(View view) {
+    updateWord();
+  }
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void nextWord(View view) {
-        updateWord();
-    }
-
-    private void updateWord() {
-        int randomNum = rand.nextInt(words.length);
-        wordView.setText(words[randomNum]);
-    }
+  private void updateWord() {
+    int randomNum = rand.nextInt(words.length);
+    wordView.setText(words[randomNum]);
+  }
 
 }
